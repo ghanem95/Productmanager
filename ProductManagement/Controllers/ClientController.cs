24b2be0c78aa9a30e87465319da734ed34642c4c
ghanem95 <ghanembahria@gmail.com>
2020-12-13 19:26:06 +0100

    Ajout de la liste des pays dans le dropdownlist

--- ProductManagement/Controllers/ClientController.cs
+++ ProductManagement/Controllers/ClientController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,13 +15,38 @@ namespace ProductManagement.Controllers
         // GET: Client
         public ActionResult Index()
         {
+            List<string> CountryList = new List<string>();
+            CultureInfo[] CInfoList = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+            foreach (CultureInfo CInfo in CInfoList)
+            {
+                RegionInfo R = new RegionInfo(CInfo.LCID);
+                if (!(CountryList.Contains(R.EnglishName)))
+                {
+                    CountryList.Add(R.EnglishName);
+                }
+            }
+
+            CountryList.Sort();
+            ViewBag.CountryList = CountryList;
             Client clt = new Client();
             ViewBag.ListClient = clt.List();
             return View();
         }
         public ActionResult AddClt()
         {
+            List<string> CountryList = new List<string>();
+            CultureInfo[] CInfoList = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+            foreach (CultureInfo CInfo in CInfoList)
+            {
+                RegionInfo R = new RegionInfo(CInfo.LCID);
+                if (!(CountryList.Contains(R.EnglishName)))
+                {
+                    CountryList.Add(R.EnglishName);
+                }
+            }
 
+            CountryList.Sort();
+            ViewBag.CountryList = CountryList;
             return View();
         }
         public PartialViewResult DeleteClt(int idclt)
@@ -32,6 +58,19 @@ namespace ProductManagement.Controllers
         }
         public PartialViewResult DetailClt(int idclt)
         {
+            List<string> CountryList = new List<string>();
+            CultureInfo[] CInfoList = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+            foreach (CultureInfo CInfo in CInfoList)
+            {
+                RegionInfo R = new RegionInfo(CInfo.LCID);
+                if (!(CountryList.Contains(R.EnglishName)))
+                {
+                    CountryList.Add(R.EnglishName);
+                }
+            }
+
+            CountryList.Sort();
+            ViewBag.CountryList = CountryList;
             Client client = new Client();
             client.Affiche(idclt);
             return PartialView("_Detailclt", client);
